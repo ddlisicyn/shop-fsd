70a0d9c0910a48ddc32f23ee966e2b7ea95c2bb7
Лисицын Даниил <daniil.lisitsyn@gazprombank.ru>
2024-11-18 22:01:24 +0300

    feat: add pagination and load more button

--- src/widgets/ProductsList/ui/Spinner.tsx
+++ src/widgets/ProductsList/ui/Spinner.tsx
@@ -1,10 +1,10 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
-export function Spinner() {
+export function Spinner({ size }: { size: string }) {
   return (
     <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
+      <CircularProgress size={size} />
     </Box>
   );
 }

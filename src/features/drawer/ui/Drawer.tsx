deb2fbfd0292a540652de1c622b4fca6fd6740c6
Лисицын Даниил <daniil.lisitsyn@gazprombank.ru>
2024-12-09 14:37:42 +0300

    minor changes

--- src/features/drawer/ui/Drawer.tsx
+++ src/features/drawer/ui/Drawer.tsx
@@ -74,7 +74,7 @@ export function DrawerModule({ open, handleDrawerClose }: DrawerModuleProps) {
           <ListItem disablePadding key={category.name + category.value}>
             <ListItemButton
               onClick={() => handleClickOnCategory(category.name)}
-              disabled={category.name === searchParamCategory}
+              disabled={category.name === searchParamCategory && !document.location.pathname.includes('detail')}
             >
               <ListItemIcon>{category.icon}</ListItemIcon>
               <ListItemText>{category.value}</ListItemText>
